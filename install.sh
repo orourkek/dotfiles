c63723d5f09b990a71849c5da6f64d5d6da0a580
Kevin O'Rourke <korourke@jeltz>
2015-06-03 16:45:15 -0700

    WIP MASSIVE UPDATE

--- install.sh
+++ install.sh
@@ -1,59 +1,134 @@
 #!/usr/bin/env bash
+#
+# Install script from:
+# https://github.com/dave-tucker/dotfiles
 
-# get into the right directory
-SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-POWERLINE_DIR="$SCRIPT_DIR/lib/powerline-shell"
-CONFIG_DIR="$SCRIPT_DIR/config"
+DOTFILES_ROOT="`pwd`"
+POWERLINE_ROOT="$DOTFILES_ROOT/lib/powerline-shell"
 
-printf "\nkorourke's dotfiles"
-printf "\n⌁⌁⌁⌁⌁⌁⌁⌁⌁⌁⌁⌁⌁⌁⌁⌁⌁⌁⌁\n"
+set -e
 
-if [ "$#" -eq 0 ]; then
-  printf "Which theme would you like?\n"
-  printf "  1. Solarized (dark)\n"
-  printf "  2. Solarized (light)\n"
-  printf "  3. Base16: Ocean (dark)\n"
-  printf "  4. Base16: Ocean (light)\n"
-  read -p "> " -n 1;
+echo ''
 
-  if [[ $REPLY == 1 ]]; then
-    THEME="solarized-dark"
-  elif [[ $REPLY == 2 ]]; then
-    THEME="solarized-light"
-  elif [[ $REPLY == 3 ]]; then
-    THEME="base16-ocean-dark"
-  elif [[ $REPLY == 4 ]]; then
-    THEME="base16-ocean-light"
-  fi
-else
-  THEME=$1
-fi
-printf "\nUsing theme '$THEME'\n"
+info () {
+  printf "  [ \033[00;34m..\033[0m ] $1"
+}
 
-# setup powerline-shell
-CONFIG="
-SEGMENTS = [
-  'set_term_title',
-  'virtual_env',
-  'username',
-  #'hostname',
-  #'datetime',
-  'ssh',
-  #'cwd',
-  'cwd-mine',
-  'read_only',
-  'git-mine',
-  #'hg',
-  #'svn',
-  #'fossil',
-  'jobs',
-  'root',
-]
-THEME='../../../themes/$THEME/powerline'"
-cd $POWERLINE_DIR
-echo "$CONFIG" > config.py
-python install.py > /dev/null
-cd -
+user () {
+  printf "\r  [ \033[0;33m?\033[0m ] $1 "
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+
+link_files () {
+  ln -s $1 $2
+  success "linked $1 to $2"
+}
+
+setup_powerline() {
+  # setup powerline-shell
+  CONFIG="
+  SEGMENTS = [
+    'set_term_title',
+    'virtual_env',
+    'username',
+    #'hostname',
+    #'datetime',
+    'ssh',
+    #'cwd',
+    'cwd-mine',
+    'read_only',
+    'git-mine',
+    #'hg',
+    #'svn',
+    #'fossil',
+    'jobs',
+    'root',
+  ]
+  THEME='../../powerline/theme-dark.py'"
+  cd $POWERLINE_ROOT
+  echo "$CONFIG" > config.py
+  python install.py > /dev/null
+  cd -
+}
+
+install_dotfiles () {
+  info 'installing dotfiles'
+
+  overwrite_all=false
+  backup_all=false
+  skip_all=false
+
+  for source in `find $DOTFILES_ROOT -maxdepth 2 -name \*.symlink`
+  do
+    dest="$HOME/.`basename \"${source%.*}\"`"
+
+    if [ -f $dest ] || [ -d $dest ]
+    then
+
+      overwrite=false
+      backup=false
+      skip=false
+
+      if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]
+      then
+        user "File already exists: `basename $source`, what do you want to do? [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
+        read -n 1 action
+
+        case "$action" in
+          o )
+            overwrite=true;;
+          O )
+            overwrite_all=true;;
+          b )
+            backup=true;;
+          B )
+            backup_all=true;;
+          s )
+            skip=true;;
+          S )
+            skip_all=true;;
+          * )
+            ;;
+        esac
+      fi
+
+      if [ "$overwrite" == "true" ] || [ "$overwrite_all" == "true" ]
+      then
+        rm -rf $dest
+        success "removed $dest"
+      fi
+
+      if [ "$backup" == "true" ] || [ "$backup_all" == "true" ]
+      then
+        mv $dest $dest\.backup
+        success "moved $dest to $dest.backup"
+      fi
+
+      if [ "$skip" == "false" ] && [ "$skip_all" == "false" ]
+      then
+        link_files $source $dest
+      else
+        success "skipped $source"
+      fi
+
+    else
+      link_files $source $dest
+    fi
+
+  done
+}
+
+setup_powerline
+install_dotfiles
 
 declare -a files=(
   "aliases"
